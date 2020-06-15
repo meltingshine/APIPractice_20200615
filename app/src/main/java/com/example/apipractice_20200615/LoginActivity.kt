e604badf97e39dc9d9280ef150cf2ab62110daac
meltingshine <meltingshine@gmail.com>
2020-06-15 20:59:37 +0900

    - 로그인 화면에서 실제로 로그인API호출

--- app/src/main/java/com/example/apipractice_20200615/LoginActivity.kt
+++ app/src/main/java/com/example/apipractice_20200615/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.apipractice_20200615
 
 import android.os.Bundle
+import com.example.apipractice_20200615.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -8,8 +9,11 @@ class LoginActivity : BaseActivity() {
     loginBtn.setOnClickListener{
         val email = emailEdt.text.toString()
         val pw = passwordEdt.text.toString()
+
+
         //입력한 ID /PW 가 진짜 회원인지 서버에 물어봐야함 (요청 필요)
 
+        ServerUtil.postRequestLogin(mContext,email,pw,null)
 
     }
     }
