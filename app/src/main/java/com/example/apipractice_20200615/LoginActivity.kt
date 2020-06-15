296f3c8d3cd325802a55ced2e807099b74823b2f
meltingshine <meltingshine@gmail.com>
2020-06-15 19:54:08 +0900

    - 로그인 버튼 눌리면 이메일과 패스워드를 받아오기 까지 해씀 - 서버요청 작업 준비중

--- app/src/main/java/com/example/apipractice_20200615/LoginActivity.kt
+++ app/src/main/java/com/example/apipractice_20200615/LoginActivity.kt
@@ -1,10 +1,17 @@
 package com.example.apipractice_20200615
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun setupEvents() {
+    loginBtn.setOnClickListener{
+        val email = emailEdt.text.toString()
+        val pw = passwordEdt.text.toString()
+        //입력한 ID /PW 가 진짜 회원인지 서버에 물어봐야함 (요청 필요)
 
+
+    }
     }
 
     override fun setValues() {
