9740f133d612225dd89ebcf3eaf79f3ab19ee562
meltingshine <meltingshine@gmail.com>
2020-06-15 22:10:14 +0900

    너무 졸ㄹ려서 못따라갔다

--- app/src/main/java/com/example/apipractice_20200615/SignUpActivity.kt
+++ app/src/main/java/com/example/apipractice_20200615/SignUpActivity.kt
@@ -2,10 +2,20 @@ package com.example.apipractice_20200615
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.apipractice_20200615.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
     override fun setupEvents() {
 
+        emailCheckBtn.setOnClickListener {
+
+            val email = emailEdt.text.toString()
+
+//            서버에 중복확인 요청
+            ServerUtil
+
+        }
     }
 
     override fun setValues() {
--- app/src/main/java/com/example/apipractice_20200615/utils/ServerUtil.kt
+++ app/src/main/java/com/example/apipractice_20200615/utils/ServerUtil.kt
@@ -3,6 +3,7 @@ package com.example.apipractice_20200615.utils
 import android.content.Context
 import android.util.Log
 import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.internal.http2.Http2Reader
 import org.json.JSONObject
 import java.io.IOException
@@ -16,6 +17,23 @@ class ServerUtil {
     companion object {
         val BASE_URL = "http://15.165.177.142" /* 호스트 주소*/
 
+//        중복 체크를 get으로 요청하는 함수
+        fun getRequestDuplicatedCheck(mContext: Context, type:String input: String handler: JsonResponseHandler?) {
+    val client = OkHttpClient
+
+
+//    어느 기능 주소로 가는지
+
+//    get방식은 주소의 paratamer를 모두 적어줘야함
+
+//    가공도니 주소를 가지고 NewBuilder로 변환 파라미터 첨부 준비
+    val urlBuilder = "${BASE_URL}/user_check".toHttpUrlOrNull()!!.newBuilder()
+}
+        uriBuilder.addEncodedQueryParmameter(Type,"tyype")
+        uriBuilder.addEncodedQueryParmameter(Value,"tyype")
+
+
+
         //        로그인 기능을 요청하는 포스트
         fun postRequestLogin(
             context: Context,
@@ -28,8 +46,14 @@ class ServerUtil {
             val client = OkHttpClient()
 
             //        어느 기능 주소로 가는지 Host와 조합해서 명시
-            val urlString = "${BASE_URL}/user"
+//            val urlString = "${BASE_URL}/user"
 
+            val urlString =  urlBuilder.Build
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .build()
 
             //        서버에 전달할 데이터를 담는 과정 (POST- 폼데이터)
             val formData = FormBody.Builder()
