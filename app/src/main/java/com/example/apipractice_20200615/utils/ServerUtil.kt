ea59886ff9ff29931e0c10442b96c0296528edb0
meltingshine <meltingshine@gmail.com>
2020-06-15 20:54:36 +0900

    - 로그인 요청 기능 작성 중 - 서버의 응답 내용 로그 찍기

--- app/src/main/java/com/example/apipractice_20200615/utils/ServerUtil.kt
+++ app/src/main/java/com/example/apipractice_20200615/utils/ServerUtil.kt
@@ -1,4 +1,80 @@
 package com.example.apipractice_20200615.utils
 
+import android.content.Context
+import android.util.Log
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+
 class ServerUtil {
+
+    //    JAVA => static에 대응되는 개념
+//    어느 객체가 하던 상관 X, 기능만 잘 되면 그만인것들을 모아두는 영역
+    companion object {
+        val BASE_URL = "http://15.165.177.142" /* 호스트 주소*/
+
+        //        로그인 기능을 요청하는 포스트
+        fun postRequestLogin(
+            context: Context,
+            email: String,
+            pw: String,
+            handler: JsonResponseHandler?
+        ) {
+
+            //        //클라이언트로 동작해주는 변수
+            val client = OkHttpClient()
+
+            //        어느 기능 주소로 가는지 Host와 조합해서 명시
+            val urlString = "${BASE_URL}/user"
+
+
+            //        서버에 전달할 데이터를 담는 과정 (POST- 폼데이터)
+            val formData = FormBody.Builder()
+                .add("email", email) //email은 위에 postRequestLogin 함수의 멤버변수
+                .add("password", pw) //pw가 멤버변수
+                .build()
+
+
+//            서버에 요청할 모든 정보를 담는 request 변수 생성
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+//                .header() //API에서 헤더를 요구하면 여기서 첨부하면 됨.
+                .build()
+
+//            ---------------------여기까진 호출할 재료 만드는 과정--------------------
+
+
+            client.newCall(request).enqueue(object : Callback {
+                //                        연결 자체에 실패한 경우
+                override fun onFailure(call: Call, e: IOException) {
+
+
+                }
+
+                //                        서버 연결에 성공한 경우
+                override fun onResponse(call: Call, response: Response) {
+
+
+//                    서버 응답 중 본문을 String으로 저장
+                    val bodyString = response.body!!.string()
+
+//                        본문 String을 Json으로 변환
+                    val json = JSONObject(bodyString)
+                    Log.d("JSON응답",json.toString())
+
+                }
+
+
+            })
+
+
+        }
+    }
+
+    //    서버 통신의 응답 내용을 Activity에 전달해주는 인터페이스
+    interface JsonResponseHandler {
+        fun onResponse(json: JSONObject)
+    }
 }
