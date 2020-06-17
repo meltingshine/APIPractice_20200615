9ef3fb41261e620accca142d2672aab4878098ab
meltingshine <meltingshine@gmail.com>
2020-06-17 20:02:41 +0900

    - 자동로그인 체크박스 /체크 결과 저장 준비 - ContextUtil.kt 추가

--- app/src/main/java/com/example/apipractice_20200615/utils/ContextUtil.kt
+++ app/src/main/java/com/example/apipractice_20200615/utils/ContextUtil.kt
@@ -4,30 +4,40 @@ import android.content.Context
 
 class ContextUtil {
 
-    companion object{
+    companion object {
 
-//        메모장(prefrence) 파일 이름에 대응 되는 개념 : 변수로 저장
+        //        메모장(prefrence) 파일 이름에 대응 되는 개념 : 변수로 저장
         val prefName = "APIPracticePref"
 
-//        저장할 항목들의 이름을 변수로 생성
+        //        저장할 항목들의 이름을 변수로 생성
         val USER_TOKEN = "USER_TOKEN"
-
+        val AUTO_LOGIN = "AUTO_LOGIN"
 //        항목에 데이터 저장(setter) or 불러오기(getter)
 
-        fun setUserToken (context:Context, token : String) {
+        fun setUserToken(context: Context, token: String) {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
-            pref.edit().putString(USER_TOKEN,token).apply()
+            pref.edit().putString(USER_TOKEN, token).apply()
         }
 
-        fun getUserToken (context: Context) : String{
+        fun getUserToken(context: Context): String {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
-            return pref.getString(USER_TOKEN,"")!!
+            return pref.getString(USER_TOKEN, "")!!
             // defValue 저장된거 없을때 돌려줄 값
 
         }
 
-    }
+        fun setAutoLogin(context: Context, autoLogin: Boolean) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+        }
 
+        fun isAutoLogin(context: Context): Boolean {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getBoolean(AUTO_LOGIN, "")!!
+
+        }
+
+    }
 
 
 }
