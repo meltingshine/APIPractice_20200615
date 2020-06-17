015ba4bc58fd26b3d6449823ada893d2c9f3f924
meltingshine <meltingshine@gmail.com>
2020-06-17 20:15:07 +0900

    - SplashActivity.kt 생성 -> 시작점으로 - 앱 시작 시 자동 로그인 가능 여부 검사

---
+++ app/src/main/java/com/example/apipractice_20200615/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.apipractice_20200615
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
